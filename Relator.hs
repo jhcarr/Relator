e46b81c43512c3e816530b94acc27c2638c49721
jhcarr <justin.h.carr@gmail.com>
2011-08-23 12:20:02 -0700

    Relator :: Version .01 Justin Carr Aug 23 2011 YO DAWG. THIS THING DOESN'T WORK YET.

--- Relator.hs
+++ Relator.hs
@@ -7,30 +7,42 @@ module Relator where
 
 -- data types
 
-type Name	= String
-data RNature	= Stranger | Acquainted | Related
+type Name = String
+
+data RNature = Stranger | Acquainted | Related
      deriving Show
-data RStance	= Hates | Dislikes | Ambivalent | Likes | Loves
+
+data RStance = Hates | Dislikes | Ambivalent | Likes | Loves
      deriving Show
-data Relation	= Relation {
-     		  person :: Name,
-     		  kind :: RNature,
-		  stance :: RStance
-		  }
+
+data Relation = Relation {
+     		person :: Name,
+     		kind :: RNature,
+		stance :: RStance
+		}
 instance Show Relation where
 	 show s = show (stance s) ++ " " ++ person s 
 	      	  	   ++ "--" ++ show (kind s)
 
-type Person	= (Name, [Relation])
+type Person = (Name, [Relation])
 
 cast = []
+
+-- Simplify extract and map function
 
 hasRelation :: Name -> Relation -> Bool
 hasRelation n r = n == person r
 
+matchesName :: Name -> Person -> Bool
+matchesName n1 (n2,_) = n1 == n2
+
 checkRelation :: Person -> Name -> Bool
 checkRelation (_,l) n = elem True $ map (hasRelation n) l 
 
+addNewChar :: [Person] -> Name -> [Person]
+addNewChar [] n = (n,[]):[]
+addNewChar l n
+	   | elem True $ map (matchesName n) l
 
 -- examples
 
@@ -39,4 +51,4 @@ y = "Frank"
 rel1 = Relation {person = y, kind = Stranger, stance = Likes}
 list = [rel1]
 
-z = (x, list)
+p1 = (x, list)
