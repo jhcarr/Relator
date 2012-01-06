bbd4b2caf53df1faaf74fa3e0ac1cb20bb118f47
jhcarr <justin.h.carr@gmail.com>
2012-01-05 19:25:35 -0800

    Started the RuleSet file that should determine how the relation junction table behaves.

---
+++ RuleSet1.hs
@@ -0,0 +1,13 @@
+--Relator RuleSet1 Module
+--Justin Carr
+--1/5/2012
+--
+--These Haskell modules have been created for Justin's own amusement and as a possible reference for anyone interested in learning some basic Haskell.
+--Justin Carr does not accept any responsibility for any damage of any kind caused by use of these modules.
+
+module RuleSet1 where
+
+import Environment
+
+-- General form for the rule set:
+-- [target persons] [relations] [changes] 
